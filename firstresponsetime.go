6cbd20371040dc819203089fcc00731625facfba
Dot-Rar <dotrar@protonmail.com>
2020-05-09 23:14:05 +0100

    first response average user

--- firstresponsetime.go
+++ firstresponsetime.go
@@ -19,7 +19,13 @@ func newFirstResponseTime(db *pgxpool.Pool) *FirstResponseTime {
 
 func (f FirstResponseTime) Schema() string {
 	return `
-CREATE TABLE IF NOT EXISTS first_response_time("guild_id" int8 NOT NULL, "ticket_id" int4 NOT NULL, "response_time" interval NOT NULL, PRIMARY KEY("guild_id", "ticket_id"));
+CREATE TABLE IF NOT EXISTS first_response_time(
+	"guild_id" int8 NOT NULL,
+	"ticket_id" int4 NOT NULL,
+	"user_id" int8 NOT NULL,
+	"response_time" interval NOT NULL,
+	PRIMARY KEY("guild_id", "ticket_id")
+);
 CREATE INDEX CONCURRENTLY IF NOT EXISTS first_response_time_guild_id ON first_response_time("guild_id");
 `
 }
@@ -51,8 +57,26 @@ func (f *FirstResponseTime) GetAverageAllTime(guildId uint64) (responseTime time
 	return
 }
 
-func (f *FirstResponseTime) Set(guildId uint64, ticketId int, responseTime time.Duration) (err error) {
-	query := `INSERT INTO first_response_time("guild_id", "ticket_id", "response_time") VALUES($1, $2, $3) ON CONFLICT("guild_id", "ticket_id") DO NOTHING;`
-	_, err = f.Exec(context.Background(), query, guildId, ticketId, responseTime)
+func (f *FirstResponseTime) GetAverageUser(guildId, userId uint64, interval time.Duration) (responseTime time.Duration, e error) {
+	query := `SELECT AVG(first_response_time.response_time) FROM first_response_time, tickets WHERE tickets.open_time > current_timestamp - $1 AND first_response_time.guild_id = $2 AND first_response_time.user_id = $3;`
+	if err := f.QueryRow(context.Background(), query, interval, guildId, userId).Scan(&responseTime); err != nil && err != pgx.ErrNoRows {
+		e = err
+	}
+
+	return
+}
+
+func (f *FirstResponseTime) GetAverageAllTimeUser(guildId, userId uint64) (responseTime time.Duration, e error) {
+	query := `SELECT AVG(first_response_time.response_time) FROM first_response_time, tickets WHERE first_response_time.guild_id = $1 AND first_response_time.user_id = $2;`
+	if err := f.QueryRow(context.Background(), query, guildId, userId).Scan(&responseTime); err != nil && err != pgx.ErrNoRows {
+		e = err
+	}
+
+	return
+}
+
+func (f *FirstResponseTime) Set(guildId, userId uint64, ticketId int, responseTime time.Duration) (err error) {
+	query := `INSERT INTO first_response_time("guild_id", "ticket_id", "user_id", "response_time") VALUES($1, $2, $3, $4) ON CONFLICT("guild_id", "ticket_id") DO NOTHING;`
+	_, err = f.Exec(context.Background(), query, guildId, ticketId, userId, responseTime)
 	return
 }
