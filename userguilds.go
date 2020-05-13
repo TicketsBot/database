2adabede35915700cfe1b60a8219c0035e6f2b2c
Dot-Rar <dotrar@protonmail.com>
2020-05-13 22:08:02 +0100

    dont use batch

--- userguilds.go
+++ userguilds.go
@@ -63,15 +63,17 @@ func (u *UserGuildsTable) Set(userId uint64, guilds []UserGuild) (err error) {
 		return
 	}
 
-	batch := &pgx.Batch{}
-	batch.Queue(`DELETE FROM user_guilds WHERE "user_id" = ANY($1);`, guildIds)
+	// Using a batch request here breaks everything. And I mean everything.
+	if _, err = u.Exec(context.Background(), `DELETE FROM user_guilds WHERE "user_id" = $1 AND NOT ("guild_id" = ANY($2));`, userId, guildIdArray); err != nil {
+		return
+	}
 
 	for _, guild := range guilds {
 		query := `INSERT INTO user_guilds("user_id", "guild_id", "name", "owner", "permissions") VALUES($1, $2, $3, $4, $5) ON CONFLICT("user_id", "guild_id") DO UPDATE SET "name" = $3, "owner" = $4, "permissions" = $5;`
-		batch.Queue(query, userId, guild.GuildId, guild.Name, guild.Owner, guild.UserPermissions)
+		if _, err = u.Exec(context.Background(), query, userId, guild.GuildId, guild.Name, guild.Owner, guild.UserPermissions); err != nil {
+			continue
+		}
 	}
-
-	_, err = u.SendBatch(context.Background(), batch).Exec()
 
 	return
 }
