63696c6f867550ab88569ccbc7ddc96963fe9b42
Dot-Rar <dotrar@protonmail.com>
2020-06-12 18:56:45 +0100

    use user id instead of bot id

--- whitelabelerrors.go
+++ whitelabelerrors.go
@@ -19,18 +19,18 @@ func (w WhitelabelErrors) Schema() string {
 	return `
 CREATE TABLE IF NOT EXISTS whitelabel_errors(
 	"error_id" serial,
-	"bot_id" int8 NOT NULL,
+	"user_id" int8 NOT NULL,
 	"error" varchar(255) NOT NULL,
-	FOREIGN KEY("bot_id") REFERENCES whitelabel("bot_id") ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY("user_id") REFERENCES whitelabel("user_id") ON DELETE CASCADE ON UPDATE CASCADE,
 	PRIMARY KEY("error_id")
 );
 `
 }
 
-func (w *WhitelabelErrors) GetRecent(botId uint64, limit int) (errors []string, e error) {
-	query := `SELECT "error" FROM whitelabel_errors WHERE "bot_id" = $1 ORDER BY "error_id" DESC LIMIT $2;`
+func (w *WhitelabelErrors) GetRecent(userId uint64, limit int) (errors []string, e error) {
+	query := `SELECT "error" FROM whitelabel_errors WHERE "user_id" = $1 ORDER BY "error_id" DESC LIMIT $2;`
 
-	rows, err := w.Query(context.Background(), query, botId, limit)
+	rows, err := w.Query(context.Background(), query, userId, limit)
 	defer rows.Close()
 	if err != nil {
 		e = err
@@ -49,8 +49,8 @@ func (w *WhitelabelErrors) GetRecent(botId uint64, limit int) (errors []string, 
 	return
 }
 
-func (w *WhitelabelErrors) Append(botId uint64, error string) (err error) {
-	query := `INSERT INTO whitelabel_errors("bot_id", "error") VALUES($1, $2);`
-	_, err = w.Exec(context.Background(), query, botId, error)
+func (w *WhitelabelErrors) Append(userId uint64, error string) (err error) {
+	query := `INSERT INTO whitelabel_errors("user_id", "error") VALUES($1, $2);`
+	_, err = w.Exec(context.Background(), query, userId, error)
 	return
 }
