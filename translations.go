306959ba00d2a7eb93b458bf5d9ff8b1de111a0b
rxdn <dotrar@protonmail.com>
2020-07-10 20:11:48 +0100

    translations set

--- translations.go
+++ translations.go
@@ -2,7 +2,7 @@ package database
 
 import (
 	"context"
-	"github.com/TicketsBot/database/translations"
+	translations "github.com/TicketsBot/database/translations"
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
 )
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS translations(
 );`
 }
 
-func (t *Translations) Get(language database.Language, id database.MessageId) (content string, e error) {
+func (t *Translations) Get(language translations.Language, id translations.MessageId) (content string, e error) {
 	if err := t.QueryRow(context.Background(), `SELECT "content" from translations WHERE "language" = $1 AND "message_id" = $2`, language, id).Scan(&content); err != nil && err != pgx.ErrNoRows {
 		e = err
 	}
@@ -35,18 +35,18 @@ func (t *Translations) Get(language database.Language, id database.MessageId) (c
 	return
 }
 
-func (t *Translations) GetAll() (messages map[database.Language]map[database.MessageId]string, e error) {
+func (t *Translations) GetAll() (messages map[translations.Language]map[translations.MessageId]string, e error) {
 	rows, err := t.Query(context.Background(), "SELECT * FROM translations;")
 	if err != nil {
 		e = err
 		return
 	}
 
-	messages = make(map[database.Language]map[database.MessageId]string)
+	messages = make(map[translations.Language]map[translations.MessageId]string)
 
 	for rows.Next() {
-		var language database.Language
-		var messageId database.MessageId
+		var language translations.Language
+		var messageId translations.MessageId
 		var value string
 
 		if err := rows.Scan(&language, &messageId, &value); err != nil {
@@ -55,11 +55,17 @@ func (t *Translations) GetAll() (messages map[database.Language]map[database.Mes
 		}
 
 		if messages[language] == nil {
-			messages[language] = make(map[database.MessageId]string)
+			messages[language] = make(map[translations.MessageId]string)
 		}
 
 		messages[language][messageId] = value
 	}
 
+	return
+}
+
+func (t *Translations) Set(language string, messageId translations.MessageId, value string) (err error) {
+	query := `INSERT INTO translations("language", "message_id", "content") VALUES($1, $2, $3) ON CONFLICT("language", "message_id") DO UPDATE SET "content" = $3;`
+	_, err = t.Exec(context.Background(), query, language, int(messageId), value)
 	return
 }
