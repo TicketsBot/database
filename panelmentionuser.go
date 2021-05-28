49534148fc63d38e3d3f5925c94c3315bdf39cf1
rxdn <29165304+rxdn@users.noreply.github.com>
2021-05-28 13:02:22 +0100

    Update panel_user_mentions

--- panelmentionuser.go
+++ panelmentionuser.go
@@ -19,26 +19,26 @@ func newPanelUserMention(db *pgxpool.Pool) *PanelUserMention {
 func (p PanelUserMention) Schema() string {
 	return `
 CREATE TABLE IF NOT EXISTS panel_user_mentions(
-	"panel_message_id" int8 NOT NULL,
+	"panel_id" int NOT NULL,
 	"should_mention_user" bool NOT NULL,
-	FOREIGN KEY("panel_message_id") REFERENCES panels("message_id") ON DELETE CASCADE ON UPDATE CASCADE,
-	PRIMARY KEY("panel_message_id")
+	FOREIGN KEY("panel_id") REFERENCES panels("panel_id") ON DELETE CASCADE ON UPDATE CASCADE,
+	PRIMARY KEY("panel_id")
 );
 `
 }
 
-func (p *PanelUserMention) ShouldMentionUser(panelMessageId uint64) (shouldMention bool, e error) {
-	query := `SELECT "should_mention_user" from panel_user_mentions WHERE "panel_message_id"=$1;`
+func (p *PanelUserMention) ShouldMentionUser(panelId int) (shouldMention bool, e error) {
+	query := `SELECT "should_mention_user" from panel_user_mentions WHERE "panel_id"=$1;`
 
-	if err := p.QueryRow(context.Background(), query, panelMessageId).Scan(&shouldMention); err != nil && err != pgx.ErrNoRows {
+	if err := p.QueryRow(context.Background(), query, panelId).Scan(&shouldMention); err != nil && err != pgx.ErrNoRows {
 		e = err
 	}
 
 	return
 }
 
-func (p *PanelUserMention) Set(panelMessageId uint64, shouldMentionUser bool) (err error) {
-	query := `INSERT INTO panel_user_mentions("panel_message_id", "should_mention_user") VALUES($1, $2) ON CONFLICT("panel_message_id") DO UPDATE SET "should_mention_user" = $2;`
-	_, err = p.Exec(context.Background(), query, panelMessageId, shouldMentionUser)
+func (p *PanelUserMention) Set(panelId int, shouldMentionUser bool) (err error) {
+	query := `INSERT INTO panel_user_mentions("panel_id", "should_mention_user") VALUES($1, $2) ON CONFLICT("panel_id") DO UPDATE SET "should_mention_user" = $2;`
+	_, err = p.Exec(context.Background(), query, panelId, shouldMentionUser)
 	return
 }
