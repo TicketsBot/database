6870580ebb66f514c0f6d0c51ba7e562538b5123
rxdn <29165304+rxdn@users.noreply.github.com>
2021-06-30 17:29:46 +0100

    Panel image_url + thumbnail_url

--- panels.go
+++ panels.go
@@ -20,6 +20,8 @@ type Panel struct {
 	WelcomeMessage  *string `json:"welcome_message"`
 	WithDefaultTeam bool    `json:"default_team"`
 	CustomId        string  `json:"-"`
+	ImageUrl        *string `json:"image_url,omitempty"`
+	ThumbnailUrl    *string `json:"thumbnail_url,omitempty"`
 }
 
 type PanelTable struct {
@@ -48,6 +50,8 @@ CREATE TABLE IF NOT EXISTS panels(
 	"welcome_message" text,
 	"default_team" bool NOT NULL,
 	"custom_id" varchar(100) NOT NULL,
+	"image_url" varchar(255),
+	"thumbnail_url" varchar(255),
 	PRIMARY KEY("panel_id")
 );
 CREATE INDEX IF NOT EXISTS panels_guild_id ON panels("guild_id");
@@ -57,13 +61,13 @@ CREATE INDEX IF NOT EXISTS panels_custom_id ON panels("custom_id");`
 
 func (p *PanelTable) Get(messageId uint64) (panel Panel, e error) {
 	query := `
-SELECT panel_id, message_id, channel_id, guild_id, title, content, colour, target_category, reaction_emote, welcome_message, default_team, custom_id
+SELECT panel_id, message_id, channel_id, guild_id, title, content, colour, target_category, reaction_emote, welcome_message, default_team, custom_id, image_url, thumbnail_url
 FROM panels
 WHERE "message_id" = $1;
 `
 
 	if err := p.QueryRow(context.Background(), query, messageId).Scan(
-		&panel.PanelId, &panel.MessageId, &panel.ChannelId, &panel.GuildId, &panel.Title, &panel.Content, &panel.Colour, &panel.TargetCategory, &panel.ReactionEmote, &panel.WelcomeMessage, &panel.WithDefaultTeam, &panel.CustomId,
+		&panel.PanelId, &panel.MessageId, &panel.ChannelId, &panel.GuildId, &panel.Title, &panel.Content, &panel.Colour, &panel.TargetCategory, &panel.ReactionEmote, &panel.WelcomeMessage, &panel.WithDefaultTeam, &panel.CustomId, &panel.ImageUrl, &panel.ThumbnailUrl,
 	); err != nil && err != pgx.ErrNoRows {
 		e = err
 	}
@@ -73,13 +77,13 @@ WHERE "message_id" = $1;
 
 func (p *PanelTable) GetById(panelId int) (panel Panel, e error) {
 	query := `
-SELECT panel_id, message_id, channel_id, guild_id, title, content, colour, target_category, reaction_emote, welcome_message, default_team, custom_id
+SELECT panel_id, message_id, channel_id, guild_id, title, content, colour, target_category, reaction_emote, welcome_message, default_team, custom_id, image_url, thumbnail_url
 FROM panels
 WHERE "panel_id" = $1;
 `
 
 	if err := p.QueryRow(context.Background(), query, panelId).Scan(
-		&panel.PanelId, &panel.MessageId, &panel.ChannelId, &panel.GuildId, &panel.Title, &panel.Content, &panel.Colour, &panel.TargetCategory, &panel.ReactionEmote, &panel.WelcomeMessage, &panel.WithDefaultTeam, &panel.CustomId,
+		&panel.PanelId, &panel.MessageId, &panel.ChannelId, &panel.GuildId, &panel.Title, &panel.Content, &panel.Colour, &panel.TargetCategory, &panel.ReactionEmote, &panel.WelcomeMessage, &panel.WithDefaultTeam, &panel.CustomId, &panel.ImageUrl, &panel.ThumbnailUrl,
 	); err != nil && err != pgx.ErrNoRows {
 		e = err
 	}
@@ -89,13 +93,13 @@ WHERE "panel_id" = $1;
 
 func (p *PanelTable) GetByCustomId(guildId uint64, customId string) (panel Panel, ok bool, e error) {
 	query := `
-SELECT panel_id, message_id, channel_id, guild_id, title, content, colour, target_category, reaction_emote, welcome_message, default_team, custom_id
+SELECT panel_id, message_id, channel_id, guild_id, title, content, colour, target_category, reaction_emote, welcome_message, default_team, custom_id, image_url, thumbnail_url
 FROM panels
 WHERE "guild_id" = $1 AND "custom_id" = $2;
 `
 
 	err := p.QueryRow(context.Background(), query, guildId, customId).Scan(
-		&panel.PanelId, &panel.MessageId, &panel.ChannelId, &panel.GuildId, &panel.Title, &panel.Content, &panel.Colour, &panel.TargetCategory, &panel.ReactionEmote, &panel.WelcomeMessage, &panel.WithDefaultTeam, &panel.CustomId,
+		&panel.PanelId, &panel.MessageId, &panel.ChannelId, &panel.GuildId, &panel.Title, &panel.Content, &panel.Colour, &panel.TargetCategory, &panel.ReactionEmote, &panel.WelcomeMessage, &panel.WithDefaultTeam, &panel.CustomId, &panel.ImageUrl, &panel.ThumbnailUrl,
 	)
 
 	switch err {
@@ -111,7 +115,7 @@ WHERE "guild_id" = $1 AND "custom_id" = $2;
 
 func (p *PanelTable) GetByGuild(guildId uint64) (panels []Panel, e error) {
 	query := `
-SELECT panel_id, message_id, channel_id, guild_id, title, content, colour, target_category, reaction_emote, welcome_message, default_team, custom_id
+SELECT panel_id, message_id, channel_id, guild_id, title, content, colour, target_category, reaction_emote, welcome_message, default_team, custom_id, image_url, thumbnail_url
 FROM panels
 WHERE "guild_id" = $1;`
 
@@ -125,7 +129,7 @@ WHERE "guild_id" = $1;`
 	for rows.Next() {
 		var panel Panel
 		if err := rows.Scan(
-			&panel.PanelId, &panel.MessageId, &panel.ChannelId, &panel.GuildId, &panel.Title, &panel.Content, &panel.Colour, &panel.TargetCategory, &panel.ReactionEmote, &panel.WelcomeMessage, &panel.WithDefaultTeam, &panel.CustomId,
+			&panel.PanelId, &panel.MessageId, &panel.ChannelId, &panel.GuildId, &panel.Title, &panel.Content, &panel.Colour, &panel.TargetCategory, &panel.ReactionEmote, &panel.WelcomeMessage, &panel.WithDefaultTeam, &panel.CustomId, &panel.ImageUrl, &panel.ThumbnailUrl,
 		); err != nil {
 			e = err
 			continue
@@ -139,12 +143,12 @@ WHERE "guild_id" = $1;`
 
 func (p *PanelTable) Create(panel Panel) (panelId int, err error) {
 	query := `
-INSERT INTO panels("message_id", "channel_id", "guild_id", "title", "content", "colour", "target_category", "reaction_emote", "welcome_message", "default_team", "custom_id")
-VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+INSERT INTO panels("message_id", "channel_id", "guild_id", "title", "content", "colour", "target_category", "reaction_emote", "welcome_message", "default_team", "custom_id", "image_url", "thumbnail_url")
+VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
 ON CONFLICT("message_id") DO NOTHING
 RETURNING "panel_id";`
 
-	err = p.QueryRow(context.Background(), query, panel.MessageId, panel.ChannelId, panel.GuildId, panel.Title, panel.Content, panel.Colour, panel.TargetCategory, panel.ReactionEmote, panel.WelcomeMessage, panel.WithDefaultTeam, panel.CustomId).Scan(&panelId)
+	err = p.QueryRow(context.Background(), query, panel.MessageId, panel.ChannelId, panel.GuildId, panel.Title, panel.Content, panel.Colour, panel.TargetCategory, panel.ReactionEmote, panel.WelcomeMessage, panel.WithDefaultTeam, panel.CustomId, panel.ImageUrl, panel.ThumbnailUrl).Scan(&panelId)
 	return
 }
 
@@ -160,11 +164,13 @@ UPDATE panels
 		"reaction_emote" = $8,
 		"welcome_message" = $9,
 		"default_team" = $10,
-		"custom_id" = $11
+		"custom_id" = $11,
+		"image_url" = $12,
+		"thumbnail_url" = $13
 	WHERE
 		"panel_id" = $1
 ;`
-	_, err = p.Exec(context.Background(), query, panel.PanelId, panel.MessageId, panel.ChannelId, panel.Title, panel.Content, panel.Colour, panel.TargetCategory, panel.ReactionEmote, panel.WelcomeMessage, panel.WithDefaultTeam, panel.CustomId)
+	_, err = p.Exec(context.Background(), query, panel.PanelId, panel.MessageId, panel.ChannelId, panel.Title, panel.Content, panel.Colour, panel.TargetCategory, panel.ReactionEmote, panel.WelcomeMessage, panel.WithDefaultTeam, panel.CustomId, panel.ImageUrl, panel.ThumbnailUrl)
 	return
 }
 
