e93123feeb47470b5f2371134479f9ff389e8076
rxdn <29165304+rxdn@users.noreply.github.com>
2021-07-09 14:53:59 +0100

    GetClaimedSinceCount

--- ticketclaims.go
+++ ticketclaims.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
+	"time"
 )
 
 type TicketClaims struct {
@@ -46,5 +47,21 @@ func (c *TicketClaims) Set(guildId uint64, ticketId int, userId uint64) (err err
 func (c *TicketClaims) Delete(guildId uint64, ticketId int) (err error) {
 	query := `DELETE FROM ticket_claims WHERE "guild_id"=$1 AND "ticket_id"=$2;`
 	_, err = c.Exec(context.Background(), query, guildId, ticketId)
+	return
+}
+
+// stats
+func (c *TicketClaims) GetClaimedSinceCount(guildId, userId uint64, interval time.Duration) (count int, e error) {
+	query := `
+SELECT COUNT(*)
+FROM ticket_claims
+INNER JOIN tickets
+ON ticket_claims.guild_id = tickets.guild_id AND ticket_claims.ticket_id = tickets.id
+WHERE ticket_claims.guild_id = $1 AND ticket_claims.user_id = $2 AND tickets.open_time > NOW() - $3;`
+
+	if err := c.QueryRow(context.Background(), query, guildId, userId, interval).Scan(&count); err != nil && err != pgx.ErrNoRows {
+		e = err
+	}
+
 	return
 }
