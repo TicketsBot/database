b9f88ef61370656538c04756ed0a3ba66aae5a2a
rxdn <29165304+rxdn@users.noreply.github.com>
2022-07-31 12:57:34 +0100

    Store user ID with messages

--- ticketlastmessage.go
+++ ticketlastmessage.go
@@ -14,6 +14,8 @@ type TicketLastMessageTable struct {
 type TicketLastMessage struct {
 	LastMessageId   *uint64
 	LastMessageTime *time.Time
+	UserId          *uint64
+	UserIsStaff     bool
 }
 
 func newTicketLastMessageTable(db *pgxpool.Pool) *TicketLastMessageTable {
@@ -29,6 +31,8 @@ CREATE TABLE IF NOT EXISTS ticket_last_message(
 	"ticket_id" int4 NOT NULL,
 	"last_message_id" int8,
 	"last_message_time" timestamptz,
+    "user_id" int8,
+	"user_is_staff" bool NOT NULL,
 	FOREIGN KEY("guild_id", "ticket_id") REFERENCES tickets("guild_id", "id"),
 	PRIMARY KEY("guild_id", "ticket_id")
 );
@@ -36,21 +40,30 @@ CREATE TABLE IF NOT EXISTS ticket_last_message(
 }
 
 func (m *TicketLastMessageTable) Get(guildId uint64, ticketId int) (lastMessage TicketLastMessage, e error) {
-	query := `SELECT "last_message_id", "last_message_time" FROM ticket_last_message WHERE "guild_id" = $1 AND "ticket_id" = $2;`
-	if err := m.QueryRow(context.Background(), query, guildId, ticketId).Scan(&lastMessage.LastMessageId, &lastMessage.LastMessageTime); err != nil && err != pgx.ErrNoRows { // defaults to nil if no rows
+	query := `
+SELECT "last_message_id", "last_message_time", "user_id", "user_is_staff"
+FROM ticket_last_message
+WHERE "guild_id" = $1 AND "ticket_id" = $2;`
+
+	if err := m.QueryRow(context.Background(), query, guildId, ticketId).Scan(
+		&lastMessage.LastMessageId,
+		&lastMessage.LastMessageTime,
+		&lastMessage.UserId,
+		&lastMessage.UserIsStaff,
+	); err != nil && err != pgx.ErrNoRows { // defaults to nil if no rows
 		e = err
 	}
 
 	return
 }
 
-func (m *TicketLastMessageTable) Set(guildId uint64, ticketId int, messageId uint64) (err error) {
+func (m *TicketLastMessageTable) Set(guildId uint64, ticketId int, messageId, userId uint64, userIsStaff bool) (err error) {
 	query := `
-INSERT INTO ticket_last_message("guild_id", "ticket_id", "last_message_id", "last_message_time")
-VALUES($1, $2, $3, NOW()) ON CONFLICT("guild_id", "ticket_id")
-DO UPDATE SET "last_message_id" = $3, "last_message_time" = NOW();`
+INSERT INTO ticket_last_message("guild_id", "ticket_id", "last_message_id", "last_message_time", "user_id", "user_is_staff")
+VALUES($1, $2, $3, NOW(), $4, $5) ON CONFLICT("guild_id", "ticket_id")
+DO UPDATE SET "last_message_id" = $3, "last_message_time" = NOW(), "user_id" = $4, "user_is_staff" = $5;`
 
-	_, err = m.Exec(context.Background(), query, guildId, ticketId, messageId)
+	_, err = m.Exec(context.Background(), query, guildId, ticketId, messageId, userId)
 	return
 }
 
