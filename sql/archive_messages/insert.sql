c63f2e0e2e277bc4f9ce7fe732b44630238e7bbc
rxdn <29165304+rxdn@users.noreply.github.com>
2023-07-17 20:13:27 +0100

    Use upsert

--- sql/archive_messages/insert.sql
+++ sql/archive_messages/insert.sql
@@ -1,2 +1,5 @@
 INSERT INTO archive_messages (guild_id, ticket_id, channel_id, message_id)
-VALUES ($1, $2, $3, $4);
+VALUES ($1, $2, $3, $4)
+ON CONFLICT (guild_id, ticket_id) DO UPDATE SET
+    channel_id = excluded.channel_id,
+    message_id = excluded.message_id;
