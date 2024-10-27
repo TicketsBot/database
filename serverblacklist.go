6a6af45954a4679e549faed926bc93dcdb852044
rxdn <29165304+rxdn@users.noreply.github.com>
2024-10-27 22:14:23 +0000

    Blacklist reasons

--- serverblacklist.go
+++ serverblacklist.go
@@ -2,6 +2,8 @@ package database
 
 import (
 	"context"
+	"errors"
+	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
@@ -19,13 +21,24 @@ func (b ServerBlacklist) Schema() string {
 	return `CREATE TABLE IF NOT EXISTS server_blacklist("guild_id" int8 NOT NULL UNIQUE, PRIMARY KEY("guild_id"));`
 }
 
-func (b *ServerBlacklist) IsBlacklisted(ctx context.Context, guildId uint64) (blacklisted bool, err error) {
-	err = b.QueryRow(ctx, `SELECT EXISTS (SELECT 1 from server_blacklist WHERE "guild_id" = $1);`, guildId).Scan(&blacklisted)
-	return
+func (b *ServerBlacklist) IsBlacklisted(ctx context.Context, guildId uint64) (bool, *string, error) {
+	query := `SELECT "reason" FROM server_blacklist WHERE "guild_id" = $1;`
+
+	var reason *string
+	if err := b.QueryRow(ctx, query, guildId).Scan(&reason); err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return false, nil, nil
+		} else {
+			return false, nil, err
+		}
+	}
+
+	return true, reason, nil
 }
 
-func (b *ServerBlacklist) Add(ctx context.Context, guildId uint64) (err error) {
-	_, err = b.Exec(ctx, `INSERT INTO server_blacklist("guild_id") VALUES($1) ON CONFLICT("guild_id") DO NOTHING;`, guildId)
+func (b *ServerBlacklist) Add(ctx context.Context, guildId uint64, reason *string) (err error) {
+	query := `INSERT INTO server_blacklist("guild_id", "reason") VALUES($1, $2) ON CONFLICT("guild_id") DO UPDATE SET "reason" = $2`
+	_, err = b.Exec(ctx, query, guildId, reason)
 	return
 }
 
